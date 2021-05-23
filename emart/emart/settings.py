523c4a1f11486b4c2b6014c1f6d5686c722c73b6
Husain Jinia <husainjinia0201@gmail.com>
2021-05-23 13:19:03 +0530

    Added navbar and fixed image rendering

--- emart/emart/settings.py
+++ emart/emart/settings.py
@@ -119,8 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = "/images/downloads/"
+MEDIA_ROOT = BASE_DIR
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
