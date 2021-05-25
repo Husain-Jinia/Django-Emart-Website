2955591cae587322da0e3c1f3fa643972ef46869
Husain Jinia <husainjinia0201@gmail.com>
2021-05-25 20:42:15 +0530

    Created Cart items page and db model for orders

--- emart/store/urls.py
+++ emart/store/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from .views import home, login, signup
+from .views.login import logout
+from .views.cart import Cart
 
 urlpatterns = [
     path('', home.Index.as_view(), name="homepage"),
     path('signup', signup.Signup.as_view(), name="signup"),
-    path('login', login.Login.as_view(),  name="login")
+    path('login', login.Login.as_view(),  name="login"),
+    path('logout', logout,  name="logout"),
+    path('cart', Cart.as_view(),  name="cart")
     
 ]
---
+++ emart/store/views/cart.py
@@ -0,0 +1,17 @@
+
+from django.core.checks.messages import Error
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.hashers import  check_password
+from store.models.customer import Customer
+from django.views import View
+from store.models.product import Product
+
+class Cart(View):
+    def get(self, request):
+        ids = list(request.session.get('cart').keys())
+        products = Product.get_products_by_id(ids)
+        return render(request, 'cart.html', {'products': products})
+
+
+       
